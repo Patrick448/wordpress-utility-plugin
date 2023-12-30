ebca32c61b051e436fddfa459a1f72d400b8085e
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-12-30 16:21:41 -0300

    admin menu option created

--- index.php
+++ index.php
@@ -11,4 +11,10 @@ function foobar_func( $atts ){
 }
 add_shortcode( 'test', 'foobar_func' );
 
+function utility_admin_menu_option(){
+    add_menu_page('Utility Plugin Page', 'Utility Plugin', 'manage_options', 'utility-plugin', 'utility_plugin_page', '', 200);
+}
+
+add_action('admin_menu', 'utility_admin_menu_option');
+
 ?>
